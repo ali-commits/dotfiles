2811e85462aa426e7398f6b03f7975257872e8ee
Ali Alrabeei <newaaa4@gmail.com>
2020-09-24 01:40:57 +0800

    update install.sh
    add p10k file

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+curl -sLfo ~/.p10k.zsh https://raw.githubusercontent.com/newaaa41/dotfiles/master/p10k.zsh
 curl -sLfo ~/.zshrc https://raw.githubusercontent.com/newaaa41/configs/master/zshrc
 curl -sLfo ~/.vimrc https://raw.githubusercontent.com/newaaa41/configs/master/vimrc
 curl -sLfo ~/.config/neofetch/config.conf https://raw.githubusercontent.com/newaaa41/dotfiles/master/neofetch --create-dirs
