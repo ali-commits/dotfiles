7c35be23a54fd623c0136ac7bf6661552007d82a
Ali Alrabeei <newaaa4@gmail.com>
2021-01-23 13:36:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
-curl -sLfo ~/.p10k.zsh https://raw.githubusercontent.com/newaaa41/dotfiles/master/p10k.zsh
+curl -sLfo ~/.p10k.zsh https://raw.githubusercontent.com/newaaa41/dotfiles/master/p10k/p10k.zsh
 curl -sLfo ~/.zshrc https://raw.githubusercontent.com/newaaa41/configs/master/zshrc
 curl -sLfo ~/.vimrc https://raw.githubusercontent.com/newaaa41/configs/master/vimrc
-curl -sLfo ~/.config/neofetch/config.conf https://raw.githubusercontent.com/newaaa41/dotfiles/master/neofetch --create-dirs
+curl -sLfo ~/.config/neofetch/config.conf https://raw.githubusercontent.com/newaaa41/dotfiles/master/neofetch/neofetch --create-dirs
+curl -sLfo ~/.config/latte/mcOS-BS-Large.layout.latte https://raw.githubusercontent.com/newaaa41/dotfiles/master/latte/mcOS-BS-Large.layout.latte --create-dirs
