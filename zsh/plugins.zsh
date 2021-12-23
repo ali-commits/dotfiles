60c5634f0869cb254920d32cca71a814e1faa2dc
Ali Alrabeei <newaaa4@gmail.com>
2021-12-23 21:15:43 +0800

    Update plugins.zsh

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -20,3 +20,13 @@
 
 source /usr/share/fzf/key-bindings.zsh
 source /usr/share/fzf/completion.zsh
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zdharma-continuum/zinit-annex-as-monitor \
+    zdharma-continuum/zinit-annex-bin-gem-node \
+    zdharma-continuum/zinit-annex-patch-dl \
+    zdharma-continuum/zinit-annex-rust
+
+### End of Zinit's installer chunk
