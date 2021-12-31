e96b8026a51d0edb67e60dccdbd2619acd7e2185
Ali Alrabeei <newaaa4@gmail.com>
2021-12-31 11:49:05 +0800

    31/12/2021

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,3 @@
-#abbreviations
 abbr -g pns="paru --noconfirm -S"
 abbr -g pn="paru --noconfirm"
 abbr -g pr="paru -Rns"
@@ -9,24 +8,13 @@ abbr -g aenv="source .venv/bin/activate"
 abbr -g md="mkdir -p"
 abbr -g gp="git add -A && git commit -v&& git push"
 abbr -g gc="git clone"
-abbr -g gs='git status'
-abbr -g la='sla'
+abbr -g gs="git status"
+abbr -g la="ls -lA"
 abbr -g sl="colorls"
 abbr -g sll="colorls -l"
 abbr -g sla="colorls -A"
 abbr -g slla="colorls -lA"
 abbr -g ll="ls -l"
-abbr -g la="ls -lA"
-abbr -g msen='trans ms:en'
-abbr -g enms='trans en:ms'
-abbr -g sp='trans -speak ms:'
-
-#aliases
-alias cat="bat"
-alias ~="cd ~"
-alias /="cd /"
-alias 0="cd ~"
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias ip="grc --colour=auto ip"
+abbr -g msen="trans ms:en"
+abbr -g enms="trans en:ms"
+abbr -g sp="trans -speak ms:"
