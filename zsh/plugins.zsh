6af192206e6cc14b0d9764799277204463160559
Ali Alrabeei <newaaa4@gmail.com>
2022-01-03 23:46:33 +0800

    3/1/2022

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -5,6 +5,24 @@
 # /___|___/_| |_| | .__/|_|\__,_|\__, |_|_| |_|___/
 #                 |_|            |___/  
 #
+
+
+### Added by Zinit's installer
+if [[ ! -f $HOME/.local/share/zinit/zinit.git/zinit.zsh ]]; then
+    print -P "%F{33} %F{220}Installing %F{33}ZDHARMA-CONTINUUM%F{220} Initiative Plugin Manager (%F{33}zdharma-continuum/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.local/share/zinit" && command chmod g-rwX "$HOME/.local/share/zinit"
+    command git clone https://github.com/zdharma-continuum/zinit "$HOME/.local/share/zinit/zinit.git" && \
+        print -P "%F{33} %F{34}Installation successful.%f%b" || \
+        print -P "%F{160} The clone has failed.%f%b"
+fi
+### Added by Zplugin's installer
+source "$HOME/.local/share/zinit/zinit.git/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+### End of Zplugin installer's chunk
+
+
+
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit light-mode for \
@@ -22,13 +40,12 @@ zinit light-mode for \
  zplugin ice wait lucid atload'_zsh_autosuggest_start'; zinit light zsh-users/zsh-autosuggestions
  zinit snippet OMZ::plugins/dirhistory/dirhistory.plugin.zsh
  zinit light zsh-users/zsh-history-substring-search
-#  zinit light ali-commits/zsh-commandEditor
+ zinit light ali-commits/zsh-commandEditor
  zinit light zsh-users/zsh-completions
  zinit light zpm-zsh/colorize
  zinit light zdharma-continuum/fast-syntax-highlighting
  zinit load zdharma-continuum/history-search-multi-word
+ zinit light MichaelAquilina/zsh-autoswitch-virtualenv
 
-source /usr/share/fzf/key-bindings.zsh
-source /usr/share/fzf/completion.zsh
 
 
