cbe65c09cd7469ca453fd738b97c52c084909f43
Ali Alrabeei <newaaa4@gmail.com>
2022-05-17 11:53:07 +0800

    install packages

--- install.sh
+++ install.sh
@@ -5,23 +5,52 @@ curl -sLfo ~/.vimrc https://raw.githubusercontent.com/ali-commits/dotfiles/maste
 curl -sLfo ~/.config/neofetch/config.conf https://raw.githubusercontent.com/ali-commits/dotfiles/master/neofetch/neofetch --create-dirs
 curl -sLfo ~/.config/latte/mcOS-BS-Large.layout.latte https://raw.githubusercontent.com/ali-commits/dotfiles/master/latte/mcOS-BS-Large.layout.latte --create-dirs
 
-export pgks=(docker docker-compose zsh grc neofetch neovim exa btm ripgrep fd bat procs tokei zoxidez)
 
-if  command -v apt &> /dev/null
-then
-    for pkg in ${pkgs[@]}; do
-        sudo apt install $pkg
-    done
-fi
+# install packages needed packages
 
-if command -v paru &> /dev/null
-    for pkg in ${pkgs[@]}; do
-        sudo paru -S $pkg
-    done
-fi
+export pkgs=(
+    docker
+    docker-compose
+    zsh
+    grc
+    neofetch
+    neovim
+    exa
+    bottom
+    ripgrep
+    fd
+    bat
+    procs
+    tokei
+    zoxide
+    bpython
+    ipython
+)
 
-if command -v pacman &> /dev/null
-    for pkg in ${pkgs[@]}; do
-        sudo paru -S $pkg
-    done
+if command -v apt >/dev/null 2>&1; then
+    apt update
+    apt install -y ${pkgs[@]}
+    echo "Installed packages using apt"
+elif command -v paru >/dev/null 2>&1; then
+    paru -Syu
+    paru -S --noconfirm --needed ${pkgs[@]}
+    echo "Installed packages using pacman"
+elif command -v pacman >/dev/null 2>&1; then
+    pacman -Syu
+    pacman -S --noconfirm --needed ${pkgs[@]}
+    echo "Installed packages using pacman"
+elif command -v dnf >/dev/null 2>&1; then
+    dnf -y update
+    dnf -y install ${pkgs[@]}
+    echo "Installed packages using dnf"
+elif command -v zypper >/dev/null 2>&1; then
+    zypper -y update
+    zypper -y install ${pkgs[@]}
+    echo "Installed packages using zypper"
+elif command -v yum >/dev/null 2>&1; then
+    yum -y update
+    yum -y install ${pkgs[@]}
+    echo "Installed packages using yum"
+else
+    echo "No package manager found"
 fi
