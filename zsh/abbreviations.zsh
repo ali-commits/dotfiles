137d5d2f2da3937c18f000d4490ffb1cbc308ce3
Ali Alrabeei <newaaa4@gmail.com>
2026-07-28 21:49:01 +0800

    fix(abbreviations): change command abbreviations from global (-g) to regular position to prevent argument corruption

--- zsh/abbreviations.zsh
+++ zsh/abbreviations.zsh
@@ -1,141 +1,209 @@
-abbr -g "s"="sudo"
-abbr -g "dn"="dnf"
-abbr -g "dni"="dnf install -y"
-abbr -g "dnr"="dnf remove -y"
-abbr -g "dnu"="dnf update -y"
-abbr -g "dns"="dnf search"
-abbr -g "dnf i"="dnf install -y"
-abbr -g "dnf r"="dnf remove -y"
-abbr -g "dnf u"="dnf update -y"
-abbr -g "dnf s"="dnf search"
-abbr -g "sy"="systemctl"
-abbr -g "sys"="sudo systemctl"
-abbr -g "systa"="sudo systemctl start"
-abbr -g "systo"="sudo systemctl stop"
-abbr -g "sysr"="sudo systemctl restart"
-abbr -g "syss"="systemctl status"
-abbr -g "syse"="sudo systemctl enable"
-abbr -g "sysd"="sudo systemctl disable"
-abbr -g "df"="df -h"
-abbr -g "free"="free -m"
-abbr -g "psa"="ps auxf"
-abbr -g "psg"="ps aux | grep -v grep | grep -i"
-abbr -g "ports"="netstat -tulpn"
-abbr -g "ipext"="curl ipinfo.io/ip"
-abbr -g "ipint"="hostname -I | cut -d' ' -f1"
-abbr -g "usage"="du -sh"
-abbr -g "total"="df -hl --total"
-abbr -g ".."="cd .."
-abbr -g "..."="cd ../.."
-abbr -g "...."="cd ../../.."
-abbr -g "~"="cd ~"
-abbr -g "-"="cd -"
-abbr -g "md"="mkdir -p"
-abbr -g "rd"="rmdir"
-abbr -g "ll"="ls -lh"
-abbr -g "la"="ls -lah"
-abbr -g "lt"="ls --tree"
-abbr -g "l."="ls -d .*"
-abbr -g "lsd"="ls -d */"
-abbr -g "dl"="cd ~/Downloads"
-abbr -g "doc"="cd ~/Documents"
-abbr -g "dt"="cd ~/Desktop"
-abbr -g "proj"="cd ~/Projects"
-abbr -g "cfg"="cd ~/.config"
-abbr -g "g"="git"
-abbr -g "gs"="git status"
-abbr -g "ga"="git add"
-abbr -g "gc"="git commit -m"
-abbr -g "gca"="git commit -am"
-abbr -g "gp"="git push"
-abbr -g "gpl"="git pull"
-abbr -g "gf"="git fetch"
-abbr -g "gco"="git checkout"
-abbr -g "gb"="git branch"
-abbr -g "gm"="git merge"
-abbr -g "gd"="git diff"
-abbr -g "gl"="git log"
-abbr -g "glog"="git log --oneline --graph"
-abbr -g "gclean"="git clean -fd"
-abbr -g "greset"="git reset --hard"
-abbr -g "gstash"="git stash"
-abbr -g "gpop"="git stash pop"
-abbr -g "gignore"="git update-index --assume-unchanged"
-abbr -g "gunignore"="git update-index --no-assume-unchanged"
-abbr -g "d"="docker"
-abbr -g "dc"="docker compose"
-abbr -g "dcu"="docker compose up -d"
-abbr -g "dcd"="docker compose down"
-abbr -g "dcl"="docker compose logs -f"
-abbr -g "dcp"="docker compose pull"
-abbr -g "dps"="docker ps"
-abbr -g "dpsa"="docker ps -a"
-abbr -g "dst"="docker stats"
-abbr -g "dex"="docker exec -it"
-abbr -g "dlg"="docker logs -f"
-abbr -g "drm"="docker rm"
-abbr -g "drmi"="docker rmi"
-abbr -g "dprune"="docker system prune -af"
-abbr -g "py"="python3"
-abbr -g "pipi"="pip install"
-abbr -g "pipir"="pip install -r requirements.txt"
-abbr -g "pipup"="pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U"
-abbr -g "penv"="python -m venv .venv"
-abbr -g "aenv"="source .venv/bin/activate"
-abbr -g "denv"="deactivate"
-abbr -g "pytest"="python -m pytest"
-abbr -g "nr"="npm run"
-abbr -g "ni"="npm install"
-abbr -g "nid"="npm install --save-dev"
-abbr -g "nig"="npm install -g"
-abbr -g "nrm"="npm remove"
-abbr -g "nrd"="npm run dev"
-abbr -g "nrb"="npm run build"
-abbr -g "nrt"="npm run test"
-abbr -g "wget"="wget -c"
-abbr -g "curl"="curl -L"
-abbr -g "ping"="ping -c 5"
-abbr -g "myip"="curl ifconfig.me"
-abbr -g "http"="python3 -m http.server"
-abbr -g "sizeof"="du -sh"
-abbr -g "disk"="df -h"
-abbr -g "memory"="free -h"
-abbr -g "processes"="ps aux"
-abbr -g "running"="ps aux | grep"
-abbr -g "h"="history"
-abbr -g "j"="jobs -l"
-abbr -g "c"="clear"
-abbr -g "path"="echo -e \${PATH//:/\\n}"
-abbr -g "now"="date +\"%T\""
-abbr -g "nowdate"="date +\"%Y-%m-%d\""
-abbr -g "timestamp"="date +\"%Y%m%d_%H%M%S\""
-abbr -g "zshrc"="$EDITOR ~/.zshrc"
-abbr -g "zshr"="source ~/.zshrc"
-abbr -g "zshenv"="$EDITOR ~/.zshenv"
-abbr -g "vimrc"="$EDITOR ~/.vimrc"
-abbr -g "tmuxrc"="$EDITOR ~/.tmux.conf"
-abbr -g "gitconfig"="$EDITOR ~/.gitconfig"
-abbr -g "p"="podman"
-abbr -g "pc"="podman-compose"
-abbr -g "pcu"="podman-compose up -d"
-abbr -g "pcd"="podman-compose down"
-abbr -g "pcl"="podman-compose logs -f"
-abbr -g "pcp"="podman-compose pull"
-abbr -g "pps"="podman ps"
-abbr -g "ppsa"="podman ps -a"
-abbr -g "pst"="podman stats"
-abbr -g "pex"="podman exec -it"
-abbr -g "pl"="podman logs -f"
-abbr -g "prm"="podman rm"
-abbr -g "prmi"="podman rmi"
-abbr -g "prun"="podman run --rm -it"
-abbr -g "pstart"="podman start"
-abbr -g "pstop"="podman stop"
-abbr -g "prestart"="podman restart"
-abbr -g "pprune"="podman system prune -af"
-abbr -g "ppod"="podman pod"
-abbr -g "pvol"="podman volume"
-abbr -g "pnet"="podman network"
-abbr -g "pbuild"="podman build"
-abbr -g "ppull"="podman pull"
-abbr -g "ppush"="podman push"
+# ~/.zsh/abbreviations.zsh
+# Shell abbreviations using the zsh-abbr plugin
+# Sourced once on first install; edit then run `abbr_reset` to reload
+
+###############################################################################
+#                         System Commands
+###############################################################################
+# Package management
+abbr "s"="sudo"
+
+# DNF package management (Fedora)
+abbr "dn"="dnf"
+abbr "dni"="dnf install -y"
+abbr "dnr"="dnf remove -y"
+abbr "dnu"="dnf update -y"
+abbr "dns"="dnf search"
+
+# System control
+abbr "sy"="systemctl"
+abbr "sys"="sudo systemctl"
+abbr "systa"="sudo systemctl start"
+abbr "systo"="sudo systemctl stop"
+abbr "sysr"="sudo systemctl restart"
+abbr "syss"="systemctl status"
+abbr "syse"="sudo systemctl enable"
+abbr "sysd"="sudo systemctl disable"
+
+# System information
+abbr "df"="df -h"
+abbr "free"="free -m"
+abbr "psa"="ps auxf"
+abbr "psg"="ps aux | grep -v grep | grep -i"
+abbr "ports"="netstat -tulpn"
+abbr "ipext"="curl ipinfo.io/ip"
+abbr "ipint"="hostname -I | cut -d' ' -f1"
+abbr "usage"="du -sh"
+abbr "total"="df -hl --total"
+
+###############################################################################
+#                         Directory Navigation & Shortcuts
+###############################################################################
+# Note: Regular (non-global) abbreviations so 'cd ..' or 'ls ~' don't get corrupted
+abbr ".."="cd .."
+abbr "..."="cd ../.."
+abbr "...."="cd ../../../.."
+abbr "md"="mkdir -p"
+abbr "rd"="rmdir"
+
+# Directory listing
+abbr "ll"="ls -lh"
+abbr "la"="ls -lah"
+abbr "lt"="ls --tree"
+abbr "l."="ls -d .*"
+abbr "lsd"="ls -d */"
+
+# Directory shortcuts
+abbr "dl"="cd ~/Downloads"
+abbr "doc"="cd ~/Documents"
+abbr "dt"="cd ~/Desktop"
+abbr "proj"="cd ~/Projects"
+abbr "cfg"="cd ~/.config"
+
+###############################################################################
+#                         Git Commands
+###############################################################################
+# Basic git operations
+abbr "g"="git"
+abbr "gs"="git status"
+abbr "ga"="git add"
+abbr "gc"="git commit -m"
+abbr "gca"="git commit -am"
+abbr "gp"="git push"
+abbr "gpl"="git pull"
+abbr "gf"="git fetch"
+abbr "gco"="git checkout"
+abbr "gb"="git branch"
+abbr "gm"="git merge"
+abbr "gd"="git diff"
+abbr "gl"="git log"
+
+# Advanced git
+abbr "glog"="git log --oneline --graph"
+abbr "gclean"="git clean -fd"
+abbr "greset"="git reset --hard"
+abbr "gstash"="git stash"
+abbr "gpop"="git stash pop"
+abbr "gignore"="git update-index --assume-unchanged"
+abbr "gunignore"="git update-index --no-assume-unchanged"
+
+###############################################################################
+#                         Docker Commands
+###############################################################################
+# Docker basics
+abbr "d"="docker"
+abbr "dc"="docker compose"
+abbr "dcu"="docker compose up -d"
+abbr "dcd"="docker compose down"
+abbr "dcl"="docker compose logs -f"
+abbr "dcp"="docker compose pull"
+
+# Docker containers
+abbr "dps"="docker ps"
+abbr "dpsa"="docker ps -a"
+abbr "dst"="docker stats"
+abbr "dex"="docker exec -it"
+abbr "dlg"="docker logs -f"
+abbr "drm"="docker rm"
+abbr "drmi"="docker rmi"
+abbr "dprune"="docker system prune -af"
+
+###############################################################################
+#                         Development Commands
+###############################################################################
+# Python
+abbr "py"="python3"
+abbr "pipi"="pip install"
+abbr "pipir"="pip install -r requirements.txt"
+abbr "pipup"="pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip install -U"
+abbr "penv"="python -m venv .venv"
+abbr "aenv"="source .venv/bin/activate"
+abbr "denv"="deactivate"
+abbr "pytest"="python -m pytest"
+
+# Node.js
+abbr "nr"="npm run"
+abbr "ni"="npm install"
+abbr "nid"="npm install --save-dev"
+abbr "nig"="npm install -g"
+abbr "nrm"="npm remove"
+abbr "nrd"="npm run dev"
+abbr "nrb"="npm run build"
+abbr "nrt"="npm run test"
+
+###############################################################################
+#                         Network Commands
+###############################################################################
+abbr "wget"="wget -c"
+abbr "curl"="curl -L"
+abbr "ping"="ping -c 5"
+abbr "myip"="curl ifconfig.me"
+abbr "http"="python3 -m http.server"
+
+###############################################################################
+#                         System Utilities
+###############################################################################
+abbr "sizeof"="du -sh"
+abbr "disk"="df -h"
+abbr "memory"="free -h"
+abbr "processes"="ps aux"
+abbr "running"="ps aux | grep"
+abbr "h"="history"
+abbr "j"="jobs -l"
+abbr "c"="clear"
+abbr "path"="echo -e \${PATH//:/\\n}"
+abbr "now"="date +\"%T\""
+abbr "nowdate"="date +\"%Y-%m-%d\""
+abbr "timestamp"="date +\"%Y%m%d_%H%M%S\""
+
+###############################################################################
+#                         Configuration Shortcuts
+###############################################################################
+abbr "zshrc"="$EDITOR ~/.zshrc"
+abbr "zshr"="source ~/.zshrc"
+abbr "zshenv"="$EDITOR ~/.zshenv"
+abbr "vimrc"="$EDITOR ~/.vimrc"
+abbr "tmuxrc"="$EDITOR ~/.tmux.conf"
+abbr "gitconfig"="$EDITOR ~/.gitconfig"
+
+###############################################################################
+#                         Podman Shortcuts
+###############################################################################
+# Podman basics
+abbr "p"="podman"
+abbr "pc"="podman-compose"
+abbr "pcu"="podman-compose up -d"
+abbr "pcd"="podman-compose down"
+abbr "pcl"="podman-compose logs -f"
+abbr "pcp"="podman-compose pull"
+
+# Podman containers
+abbr "pps"="podman ps"
+abbr "ppsa"="podman ps -a"
+abbr "pst"="podman stats"
+abbr "pex"="podman exec -it"
+abbr "pl"="podman logs -f"
+abbr "prm"="podman rm"
+abbr "prmi"="podman rmi"
+abbr "prun"="podman run --rm -it"
+abbr "pstart"="podman start"
+abbr "pstop"="podman stop"
+abbr "prestart"="podman restart"
+
+# Podman system
+abbr "pprune"="podman system prune -af"
+abbr "ppod"="podman pod"
+abbr "pvol"="podman volume"
+abbr "pnet"="podman network"
+abbr "pbuild"="podman build"
+abbr "ppull"="podman pull"
+abbr "ppush"="podman push"
+
+###############################################################################
+#                         Global Redirection Helpers (Only true globals)
+###############################################################################
+# Use -g ONLY for pipeline and redirection snippets
+abbr -g "G"="| grep"
+abbr -g "L"="| less"
+abbr -g "NE"="2>/dev/null"
+abbr -g "DN"=">/dev/null 2>&1"
